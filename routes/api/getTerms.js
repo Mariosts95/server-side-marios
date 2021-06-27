5372105866ab12d7d43c4330855dc3b8be37c58a
Marios Tsioutsis <72659932+Mariosts95@users.noreply.github.com>
2021-06-27 19:54:44 +0300

    Cleaner code

--- routes/api/getTerms.js
+++ routes/api/getTerms.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
-const fetch = require('node-fetch');
 const { getTerms } = require('../../services/terms');
 
 router.get('/', (req, res) => {
   const { page, size } = req.query;
-  getTerms(page, size).then(({data})=>{res.send(data._embedded.terms)}).catch(err=>{console.log(err)});
+  getTerms(page, size)
+    .then(({ data }) => {
+      res.send(data._embedded.terms);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-
 
 module.exports = router;
--- services/terms.js
+++ services/terms.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
-
-const getTerms = async (page , size) => {
-    return await axios.get(`https://www.ebi.ac.uk/ols/api/ontologies/efo/terms/?page=${page}&size=${size}`)
+// get terms from external API as a promise using axios
+const getTerms = async (page, size) => {
+  return await axios.get(`https://www.ebi.ac.uk/ols/api/ontologies/efo/terms/?page=${page}&size=${size}`);
 };
 
-module.exports = {getTerms};
+module.exports = { getTerms };
